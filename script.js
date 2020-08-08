0989c61ca0e239aee0828f9d72aefcb16c287b95
Elliott Jones <jones.elliott@outlook.com>
2020-08-08 09:12:39 -0700

    Formatted ajax response to display current city info

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 // Open Weather API Key 4471d8a288b1158a16e4a8e8f56a3e35
 
-
-
-
-
-var cityName = "";
-var zipCode = "";
-var stateCode = "";
-var countryCode = "";
-
-// API Key
-var apiKey = "4471d8a288b1158a16e4a8e8f56a3e35";
-// Building the query URL
-var queryURL = "https:\\api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + zipCode + "," + stateCode + "," + countryCode + "&appid=" + apiKey;
 // Automatically build a query URL based on user's location when site is loaded.
-var autoQueryURL = "api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}"
+// var autoQueryURL = "api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}"
 
 $('#search-button').on('click', function() {
     event.preventDefault();
-    console.log("click");
-    cityName = $('#search-input').val();
+    var cityName = $('#search-input').val();
     console.log(cityName);
-    getOpenWeather();
-});
 
-// Function to run the ajax function
-function getOpenWeather() {
+    var apiKey = "4471d8a288b1158a16e4a8e8f56a3e35";
+    // Building the query URL
+    var queryURL = "https:\\api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey;
+
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
         console.log(response);
 
-        var long = response.coord.lon;
-        console.log(long);
+        // Display city name, date, & weather icon
+        $('#city-output').text(cityName + " (" + moment().format('M/D/YYYY) '));
+        $('#wicon').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
+
+        console.log(response.main.temp);
+        // Display the current temperature
+        $('#temp-output').text('Temperature: ' + response.main.temp + String.fromCharCode(176) + "F");
+        // Display current humidity
+        $('#humidity-output').text('Humidity: ' + response.main.humidity + '%');
+        // Display current windspeed
+        $('#wind-output').text('Wind Speed: ' + response.wind.speed + ' MPH');
+
+        var cityLat  = response.coord.lat;
+        var cityLon  = response.coord.lon;
+
+        // Building query URL to retrieve UV Index based on city searched
+        var queryUVURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + cityLat + "&lon=" + cityLon;
+
+        $.ajax({
+            url: queryUVURL,
+            method: "GET",
+        }).then(function (response) {
+            console.log(response);
+            // $('#uv0output').text('UV Index: ' + );
+        });
     });
-}
+});
+
