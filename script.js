e6a5289a182f63a26ab0d384b5bfa5bea9cee0da
Elliott Jones <jones.elliott@outlook.com>
2020-08-08 16:06:47 -0700

    Added utility to auto display weather based on user location when page is loaded

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 // Open Weather API Key 4471d8a288b1158a16e4a8e8f56a3e35
 
-// Automatically build a query URL based on user's location when site is loaded.
-// var autoQueryURL = "api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}"
+// Generate Weather based on user's location when they access the page
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+  
+function success(pos) {
+    var crd = pos.coords;
 
+    // Get the user's coordinates
+    var userLat = pos.coords.latitude;
+    console.log("My Lat: " + userLat);
+    
+    var userLon = pos.coords.longitude;
+    console.log("My Lon: " + userLon);
+
+    // Get the local weather based on  the user's coordinates
+    var apiKey = "4471d8a288b1158a16e4a8e8f56a3e35";
+    //Automatically build a query URL based on user's location when site is loaded.
+    var autoQueryURL = "https:\\api.openweathermap.org/data/2.5/weather?lat=" + userLat + "&lon=" + userLon + "&units=imperial&appid=" + apiKey;
+
+    console.log(autoQueryURL);
+
+    $.ajax({
+        url: autoQueryURL,
+        method: "GET",
+    }).then(function (response) {
+        autoRenderWeather(response);
+    });
+}
+
+// Console log error if userlocation doesnt work
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+// Send request for user's location
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+// Event listener for city search button
 $('#search-button').on('click', function() {
     event.preventDefault();
     var cityName = $('#search-input').val();
     console.log(cityName);
 
-    var apiKey = "4471d8a288b1158a16e4a8e8f56a3e35";
     // Building the query URL
     var queryURL = "https:\\api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey;
 
@@ -61,3 +98,93 @@ $('#search-button').on('click', function() {
     });
 });
 
+// Render the weather to the page
+function renderWeather(response) {
+    // Display city name, date, & weather icon
+    $('#city-output').text(cityName + " (" + moment().format('M/D/YYYY) '));
+    $('#wicon').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
+
+    console.log(response.main.temp);
+    // Display the current temperature
+    $('#temp-output').text('Temperature: ' + response.main.temp + String.fromCharCode(176) + "F");
+    // Display current humidity
+    $('#humidity-output').text('Humidity: ' + response.main.humidity + '%');
+    // Display current windspeed
+    $('#wind-output').text('Wind Speed: ' + response.wind.speed + ' MPH');
+
+    // Get the lat & lon values
+    var cityLat  = response.coord.lat;
+    var cityLon  = response.coord.lon;
+
+    // Building query URL to retrieve UV Index based on city searched
+    var queryUVURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + cityLat + "&lon=" + cityLon;
+
+    $.ajax({
+        url: queryUVURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        var uvIndex = response.value;
+        var uvOutput = $('#uv-output')
+        uvOutput.text(uvIndex);
+        // Change background color based on UV Index
+        if (uvIndex < 3) {
+            uvOutput.attr('style', 'background-color: green;')
+        } else if (uvIndex < 6) {
+            uvOutput.attr('style', 'background-color: yellow;')
+        } else if (uvIndex < 8) {
+            uvOutput.attr('style', 'background-color: orange;')
+        } else if (uvIndex < 11) {
+            uvOutput.attr('style', 'background-color: red;')
+        } else {
+            uvOutput.attr('style', 'background-color: purple;')
+        }
+    });
+}
+
+// Auto Render the weather
+function autoRenderWeather(response) {
+    console.log(response);
+    var apiKey = "4471d8a288b1158a16e4a8e8f56a3e35";
+    var userCity = response.name;
+    // Display city name, date, & weather icon
+    $('#city-output').text(userCity + " (" + moment().format('M/D/YYYY) '));
+    $('#wicon').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
+
+    console.log(response.main.temp);
+    // Display the current temperature
+    $('#temp-output').text('Temperature: ' + response.main.temp + String.fromCharCode(176) + "F");
+    // Display current humidity
+    $('#humidity-output').text('Humidity: ' + response.main.humidity + '%');
+    // Display current windspeed
+    $('#wind-output').text('Wind Speed: ' + response.wind.speed + ' MPH');
+
+    // Get the lat & lon values
+    var cityLat  = response.coord.lat;
+    var cityLon  = response.coord.lon;
+
+    // Building query URL to retrieve UV Index based on city searched
+    var queryUVURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + cityLat + "&lon=" + cityLon;
+
+    $.ajax({
+        url: queryUVURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        var uvIndex = response.value;
+        var uvOutput = $('#uv-output')
+        uvOutput.text(uvIndex);
+        // Change background color based on UV Index
+        if (uvIndex < 3) {
+            uvOutput.attr('style', 'background-color: green;')
+        } else if (uvIndex < 6) {
+            uvOutput.attr('style', 'background-color: yellow;')
+        } else if (uvIndex < 8) {
+            uvOutput.attr('style', 'background-color: orange;')
+        } else if (uvIndex < 11) {
+            uvOutput.attr('style', 'background-color: red;')
+        } else {
+            uvOutput.attr('style', 'background-color: purple;')
+        }
+    });
+}
